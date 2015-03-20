bffd64ed2cfe05489af506b59fc5b06c5bcd1b6f
Jan Godziatkowski <ph.godzio@gmail.com>
2015-03-20 12:26:23 +0100

    Utworzenie testu sprawdzajacego zachowanie metody issuance z klasy BookKeeper
    Tworzymy struktury danych typow ProductData, RequestItem, ClientData, oraz dodajemy dwa razy stworzony RequestItem do InvoiceRequest.
    Tworzymy obiekty mockowane dla typow InvoiceFactory oraz TaxPolicy.
    Sprawdzamy czy metoda calculateTax z klasy TaxPolicy zostanie wywolana dwa razy dla dwoch elementow na liscie InvoiceRequest

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -44,4 +44,27 @@ public class BookKeeperTest {
         Assert.assertThat(invoice.getItems().size(), is(1));
     }
 
+    @Test
+    public void issuance_requestForInvoiceWithTwoElements_callTaxPolicyCalculateTaxTwoTimes() {
+        //Given
+        InvoiceFactory mockInvoiceFactory = mock(InvoiceFactory.class);
+        TaxPolicy mockTaxPolicy = mock(TaxPolicy.class);
+
+        ProductData productData = new ProductData(Id.generate(), new Money(1), "makowiec", ProductType.FOOD, new Date());
+        RequestItem requestItem = new RequestItem(productData, 1, new Money(7));
+        ClientData clientData = new ClientData(Id.generate(), "godzio");
+        InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+        invoiceRequest.add(requestItem);
+        invoiceRequest.add(requestItem);
+
+        BookKeeper bookKeeper = new BookKeeper(mockInvoiceFactory);
+
+        //When
+        when(mockTaxPolicy.calculateTax((ProductType) any(), (Money) any())).thenReturn(new Tax(new Money(1), ""));
+        when(mockInvoiceFactory.create(clientData)).thenReturn(new Invoice(Id.generate(), clientData));
+        bookKeeper.issuance(invoiceRequest, mockTaxPolicy);
+        //Then
+
+        verify(mockTaxPolicy, times(2)).calculateTax((ProductType) any(), (Money) any());
+    }
 }
